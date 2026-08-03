219a1702ae0b08149fb71efb4d3d1cd5f09f402a
Rahid Haque <ontirahid80@gmail.com>
2026-08-03 19:16:14 +0600

    Lib Prisma Error Fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../prisma/generated/prisma/client";
+import { config } from "../config";
 
 
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${config.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
