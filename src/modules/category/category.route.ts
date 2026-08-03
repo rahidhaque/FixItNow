db7598cb69ac89f4ce199c24c055247fa4741a1d
Rahid Haque <ontirahid80@gmail.com>
2026-08-04 00:54:33 +0600

    Admin access added to post service & post category

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addCategory, getCategory } from "./category.controller";
+import auth from "../../middleware/auth";
 
 const categoryRouter = Router();
 
 categoryRouter.post("/", addCategory);
-categoryRouter.get("/", getCategory);
+categoryRouter.get("/", auth("ADMIN"), getCategory);
 
 export default categoryRouter;
--- src/modules/service/service.route.ts
+++ src/modules/service/service.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { addService, getServices } from "./service.controller";
+import auth from "../../middleware/auth";
 
 export const serviceRouter = Router();
 
-serviceRouter.post("/service", addService);
+serviceRouter.post("/service", auth("ADMIN"), addService);
 serviceRouter.get("/services", getServices);
